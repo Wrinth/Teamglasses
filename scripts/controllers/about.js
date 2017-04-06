7a676031a488447e9b6cd2be117e7c6ace517136
Wrinth <johnl4112@gmail.com>
2017-04-05 19:53:56 -0700

    Third commit

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teamgrassesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the teamgrassesApp
+ */
+angular.module('teamgrassesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teamgrassesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the teamgrassesApp
+ */
+angular.module('teamgrassesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
